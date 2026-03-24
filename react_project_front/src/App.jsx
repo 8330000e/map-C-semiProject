61abe5ecd1113565b17aae8b71dfc4afce615880
김가연 <gyn38k@gmail.com>
2026-03-25 08:23:46 +0900

    setting

--- react_project_front/src/App.jsx
+++ react_project_front/src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import "./font.css";
+import Footer from "./components/commons/Footer";
+import Header from "./components/commons/Header";
+import Main from "./main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="wrap">
+      <Header />
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
