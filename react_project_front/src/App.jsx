ae54776491bfb724b30857c2cc099f163cb4c49d
gh07003 <gh07003@gmail.com>
2026-04-03 11:17:26 +0900

    test

--- react_project_front/src/App.jsx
+++ react_project_front/src/App.jsx
@@ -19,7 +19,7 @@ import PaymentSuccess from "./pages/payment/PaymentSuccess";
 import PaymentFail from "./pages/payment/PaymentFail";
 import JoinPage from "./pages/member/JoinPage";
 import LoginPage from "./pages/member/LoginPage";
-import TestHeader from "./components/commons/TestHeader";
+
 import AdminPage from "./pages/admin/AdminPage";
 
 function App() {
@@ -46,7 +46,6 @@ function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/join" element={<JoinPage />}></Route>
           <Route path="/members/login" element={<LoginPage />}></Route>
-          <Route path="/test-header" element={<TestHeader />} />
           <Route path="/admin/*" element={<AdminPage />} />
         </Routes>
       </main>
