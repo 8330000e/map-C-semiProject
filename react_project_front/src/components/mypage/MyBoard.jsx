46e6127e50e169e86584beb890ef9ee205cf4ef1
mjw012405170509-max <mjw012405170509@gmail.com>
2026-04-06 17:08:49 +0900

    04-06:0508
    asd

--- react_project_front/src/components/mypage/MyBoard.jsx
+++ react_project_front/src/components/mypage/MyBoard.jsx
@@ -3,28 +3,22 @@ import { Input } from "../ui/Form";
 import SearchIcon from "@mui/icons-material/Search";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { memberId } from "../../store/useAuthStore";
+import useAuthStore from "../../store/useAuthStore";
 
 const MyBoard = () => {
+  const { memberId } = useAuthStore();
   const [boardSearch, setBoardSearch] = useState("");
-  const [searchBoard, setSearchBoard] = useState();
+  const [searchBoard, setSearchBoard] = useState("shit");
   const [filter, setFilter] = useState(2);
-  const [boardList, setBoardList] = useState({
-    memberId: "memberId",
-    boardNo: "",
-    memberNickName: "",
-    boardTitle: "",
-    boardThumb: "",
-    boardDate: "",
-    readCount: "",
-    searchBoard: searchBoard,
-    filter: filter,
-  });
+  const [boardList, setBoardList] = useState([]);
   useEffect(() => {
     axios
-      .get(`${import.meta.env.VITE_BACKSERVER}/boards/${memberId}`, boardList)
+      .get(
+        `${import.meta.env.VITE_BACKSERVER}/boards/${memberId}?searchBoard=${searchBoard}&filter=${filter}`,
+      )
       .then((res) => {
         console.log(res.data);
+        setBoardList(res.data);
       })
       .catch((err) => {
         console.log(err);
@@ -53,23 +47,28 @@ const MyBoard = () => {
         <div className={styles.myboard_filter}>
           <select
             value={filter}
-            onClick={(e) => {
+            onChange={(e) => {
               setFilter(e.target.value);
             }}
           >
-            <option value={1}>오래된순</option>
-            <option value={2}>최신순</option>
+            <option value={1}>최신순</option>
+            <option value={2}>오래된순</option>
             <option value={3}>최소조회수</option>
             <option value={4}>최대조회수</option>
           </select>
         </div>
         <div className={styles.myboard_content_wrap}>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+          {boardList.map((board, index) => {
+            return (
+              <ul key={board.boardNo}>
+                <li>{board.boardNo}</li>
+                <li>{memberId}</li>
+                <li>{board.boardDate}</li>
+                <li>{board.boardTitle}</li>
+                <li>{board.readCount}</li>
+              </ul>
+            );
+          })}
         </div>
       </div>
     </div>
