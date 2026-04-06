1bf20e5b951340e66cd2905ce358bf4a0cf773e6
gh07003 <gh07003@gmail.com>
2026-04-06 14:06:38 +0900

    관리자 공지사항 삭제 구현

--- react_project_back/src/main/java/kr/co/iei/admin/controller/AdminController.java
+++ react_project_back/src/main/java/kr/co/iei/admin/controller/AdminController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +50,11 @@ public class AdminController {
 		DashData dashData = adminService.getDashData();
 		System.out.println(dashData);
 		return ResponseEntity.ok(dashData);
+	}
+	
+	@DeleteMapping(value="notice/{noticeNo}")
+	public ResponseEntity<?> deleteNotice(@PathVariable Integer noticeNo) {
+		int result = adminService.deleteNotice(noticeNo);
+		return ResponseEntity.ok(result);
 	}
 }
