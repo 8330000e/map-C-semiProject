f9e16aa3dd77f1ae28c05e287eb7e2ff475078ff
BC215 <dlqudckd3@gmail.com>
2026-04-13 09:35:53 +0900

    --

--- react_project_back/src/main/java/kr/co/iei/store/model/vo/StoreBoard.java
+++ react_project_back/src/main/java/kr/co/iei/store/model/vo/StoreBoard.java
@@ -25,14 +25,4 @@ public class StoreBoard {
 
     private Date createdAt;
     private Date updatedAt;
-
-    // Lombok should generate this, but add explicitly for build stability
-    public String getProductStatus() {
-        return productStatus;
-    }
-
-    public void setProductStatus(String productStatus) {
-        this.productStatus = productStatus;
-    }
-
 }
