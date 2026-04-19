bc30418b0b68bec30cc48c5fc086e27302429c61
김가연 <144993449+8330000e@users.noreply.github.com>
2026-04-19 10:30:18 +0900

    오타수정

--- react_project_front/src/pages/MapCommunityPage/MapCommunityPage.jsx
+++ react_project_front/src/pages/MapCommunityPage/MapCommunityPage.jsx
@@ -62,7 +62,6 @@ const MapCommunityPage = () => {
           <ArrowBackIosOutlinedIcon />
           <span>메인으로 돌아가기</span>
         </div>
-        ㅣ
       </div>
       <div className={styles.container}>
         <div className={styles.left}>
