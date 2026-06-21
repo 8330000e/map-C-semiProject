237e049f0e41ff22506cfafeb144c9e25ce1fd28
Ubuntu <ubuntu@ip-172-31-34-197.ap-northeast-2.compute.internal>
2026-06-21 06:46:25 +0000

    이거왜안끝나

--- react_project_back/src/main/java/kr/co/iei/WebConfig.java
+++ react_project_back/src/main/java/kr/co/iei/WebConfig.java
@@ -4,6 +4,7 @@ import kr.co.iei.utils.AdminInterceptor;
 import kr.co.iei.utils.MemberStatusInterceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -26,14 +27,19 @@ public class WebConfig implements WebMvcConfigurer {
 		// allowCredentials(true)를 사용할 때는 allowedOrigins("*")를 사용할 수 없습니다.
 		// 브라우저는 자격 증명 포함 응답에서 Access-Control-Allow-Origin을 '*'로 설정하는 것을 허용하지 않습니다.
 		// 따라서 명시적 origin 목록이나 allowedOriginPatterns를 사용해야 합니다.
-		registry.addMapping("/api/**")
-			.allowedOrigins("https://d2g15isq25ks7i.cloudfront.net")
+		registry.addMapping("**")
+			.allowedOrigins("https://d2g15isq25ks7i.cloudfront.net/")
 			.allowedOriginPatterns("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:9999")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true);
 	}
-	
+
+	@Override
+        public void configurePathMatch(PathMatchConfigurer configurer) {
+            // 모든 컨트롤러의 매핑 앞에 자동으로 /api를 붙입니다.
+            configurer.addPathPrefix("/api", c -> true);
+        }
 	
 	 @Override
 	public void addInterceptors(InterceptorRegistry registry) {
