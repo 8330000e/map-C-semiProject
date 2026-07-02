ee0ca812e400a96d14da5299820457b0ec2b01a4
김가연 <gyn38k@gmail.com>
2026-07-02 10:38:26 +0900

    외않되ㅜ

--- react_project_front/src/pages/MapCommunityPage/MapCommunityPage.jsx
+++ react_project_front/src/pages/MapCommunityPage/MapCommunityPage.jsx
@@ -330,7 +330,7 @@ const Map = ({
       ),
       map: map,
       icon: {
-        content: `<div style="position: relative; width: 100%; z-index:99999;"><img loading="lazy" decoding="async" src=${marker} style="width: 30px; margin: 0px; padding: 0px; border: 0px solid transparent; display: block; min-width: 30px; min-height: none; z-index=99999; -webkit-user-select: none; position: absolute; left: 0px; top: 0px;"></div>`,
+        content: `<div style="position: relative; width: 100%; z-index:99999;"><img loading="lazy" decoding="async" src="../../assets/img/marker.png" style="width: 30px; margin: 0px; padding: 0px; border: 0px solid transparent; display: block; min-width: 30px; min-height: none; z-index=99999; -webkit-user-select: none; position: absolute; left: 0px; top: 0px;"></div>`,
         size: new naver.maps.Size(22, 35),
         anchor: new naver.maps.Point(11, 35),
       },
